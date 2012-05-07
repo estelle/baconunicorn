1fd5c30d5c456597b9d59238395ae5ded6439e7d
Anna Billstrom <banane@gmail.com>
2012-05-07 15:26:25 -0700

    commented receiving server code

--- server/receiveMMS.php
+++ server/receiveMMS.php
@@ -1,6 +1,25 @@
 <?php
+/*
+	this script's path should be entered in the app definition of at&t dev portal
+	i.e. banane.com/baconunciorn/server/receiveMMS.php
+	
+	to implement,
+		1. create write-able log in tmp/receivingmms.log
+		2. create writeable gallery.json in sencha's data/assets/gallery dir.
+		3. grab test data file test_mms.txt and un-comment (will make life lots easier)
+		4. test by going direct to server/receiveMMS.php, will print out json_message, then check out the client
+		"client/"
+		should display the image in test object (a blue tree)
+		
+		That should be it- feel free to disable the writing to the MoMmsImages dir, 
+		but found it useful to debug.
+	-ab mon may 7th
+*/
    
 require("dBug.php");
+/* great debug lib for PHP, available on this domain -ab mon may 7th */
+
+/* make sure to make writeable on local sys */
 $fh = fopen("tmp/receivingmms.log","a+");
 $dateTime = date('Y/m/d G:i:s');
 fwrite($fh,"\n\n***** $dateTime getting post\n");
@@ -78,6 +97,17 @@ foreach ( $boundaries_parts as $mime_part ){
       }
       if ( $filename != null ){
 				//Save file 
+				
+				/* 
+					ab mon may 7th
+					bet you're wondering why this is here... the content type is looped through earlier,
+					and this is where at&t put this filename stuff. thing is, it's a long string
+					and badly pattern matched, and doesn't write to o/s well. 
+					I wrote a new one ("@Filename=: ((.)*\;))) but while it printed out, wasn't sure why didn't work with other processes.
+					Instead, pop into array delimited by spaces
+					and grab first element. Should probably tighten up reg-ex's instead, of course.
+				
+				*/
 				if($content_type == "image/jpeg" || $content_type == "image/png"){
 					new dBug($filename);
 					new dBug($json_full_filename);
@@ -122,17 +152,14 @@ if( $messages !=null ){
     $old_message = array_shift($messages);
     // remove old message folder 
   }
-  
-    /* ab  mon may 7th
-  	do same for json client side - pop into array, stringify, overwrite old db file
-  	would be better w/ a real db
-  */
-
-  
  }else{
     $messages = array($message);
  }
 
+  /* ab  mon may 7th
+  	do same for json client side - pop into array, stringify, overwrite old db file
+  	would be better w/ a real db
+  */
 if($json_message != null){
   new dBug($json_message);
   fwrite($fh,"in write to db\n");
